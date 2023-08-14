66b10bb743b247cc7553541254bf9f07003d5057
Fawl3r <Fawl3r@gmail.com>
2023-08-14 13:00:01 -0500

    change

---
+++ scripts/bundlePack.mjs
@@ -0,0 +1,238 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import dotenv from "dotenv";
+dotenv.config();
+
+(async () => {
+// Initialize with Private Key
+const sdk = ThirdwebSDK.fromPrivateKey(process.env.PRIVATE_KEY, "binance");
+
+// Initialize with Secret Key
+const readOnlySdk = new ThirdwebSDK("binance", {
+    secretKey: "YOUR_SECRET_KEY"
+});
+
+    
+
+    const packAddress ="0x41C3778a435CD8D911cD5BbE8fDc50438115BFDE";
+    const cardAddress ="0xB8a33200A4a09d8c5F5313D6a19A9609B916479e";
+
+    const pack = sdk.getContract(packAddress, "pack");
+
+    const card = sdk.getContract(cardAddress, "edition");
+    await (await card).setApprovalForAll(packAddress, true);
+    console.log("Approved card contract to transfer cards to pack contract");
+
+    const packImage = "ipfs://Qmc8iWUVg2RZBJLbzhBU9vxDt1hVm9kXzK5FujAtgoRJgo/fawl3r_space_age_loot_chest_5a2de240-ae57-493f-abd1-24cbe1a16e2b.png";
+
+    console.log("Creating pack");
+    const createPacks = (await pack).create({
+        packMetadata: {
+            name: "Starter Pack",
+            description: "F3 Warrior Lootbox",
+            image: packImage,
+        },
+        erc1155Rewards: [
+            {
+                contractAddress: cardAddress,
+                tokenId: 1,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 2,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 3,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 4,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 5,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 6,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 7,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 8,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 9,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 10,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 11,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 12,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 13,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 14,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 15,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 16,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 17,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 18,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 19,
+                quantityPerReward: 1,
+                totalRewards:50,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 20,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 21,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 22,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 23,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 24,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 25,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 27,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 28,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 29,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 30,
+                quantityPerReward: 1,
+                totalRewards:12,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 31,
+                quantityPerReward: 1,
+                totalRewards:2,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 32,
+                quantityPerReward: 1,
+                totalRewards:2,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 33,
+                quantityPerReward: 1,
+                totalRewards:2,
+            },
+            {
+                contractAddress: cardAddress,
+                tokenId: 34,
+                quantityPerReward: 1,
+                totalRewards:2,
+            }
+        ],
+        rewardsPerPack: 2,
+    });
+
+    console.log("Packs created");
+})();
